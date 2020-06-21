893478def20cce92348ce32c533eb7cf76b82405
Marcos <marcosrabcob@gmail.com>
2020-06-21 19:10:32 +0200

    nvar v1

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,17 @@
-<script>
-	export let name;
-</script>
+<!-- <script> </script> -->
+<!-- JAVASCRIPT/LOGIC -->
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<!-- <style></style> -->
+<!-- CSS/STYLING -->
+<!-- HTML -->
+<!-- HARD TO MANAGE -->
+<!-- HARD TO RE-USE -->
+<nav class="nav">
+	<div class="nav-center">
+		<h1 class="nav-title">Budget calculator</h1>
+		<button type="button" class="nav-btn">
+		<i class="far fa-plus-square" > add item </i>
+		</button>
+	</div>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+</nav>
