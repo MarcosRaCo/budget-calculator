e905f04fae8a45c87dfa8103a77ffbce5733cfad
Marcos <marcosrabcob@gmail.com>
2020-06-22 22:35:25 +0200

    mostrar 4 expenses v2

--- src/Expenses.svelte
+++ src/Expenses.svelte
@@ -1,7 +1,11 @@
 <script>
+    //export let id;
     export let expense;
+    let {name, amount,id} = expense;
+    
 </script>
 <div>
-    <h2>title : {expense.name}</h2>
+    <h2>title: {expense.name}</h2>
+    <h2>amount {expense.amount}</h2>
 </div>
 
