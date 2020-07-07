f37e3c0df2a21a5ee785123604b96fc005ab3d48
Marcos <marcosrabcob@gmail.com>
2020-07-07 20:30:37 +0200

    Revert "clear expenses button"
    This reverts commit 04f3d71f65033d6ae13c5710eccaa5dd820b9ea4.

--- src/App.svelte
+++ src/App.svelte
@@ -7,17 +7,10 @@ import expensesData from '../public/expenses';
 
 // variables 
 let expenses = [...expensesData];
-//reactive
-$: total = expenses.reduce((acc,curr)=>{
-    return (acc += curr.amount);
-})
+
 //funtions
 function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
-
-}
-function clearExpenses(id) {
-    expenses = [];
 
 }
 </script>
@@ -25,5 +18,4 @@ function clearExpenses(id) {
 <Navbar />
 <main class="content">
     <ExpensesList {expenses} {removeExpense}/>
-    <button type="button" class="btn btn-primary btn-block" on:click={clearExpenses}>clear expenses</button>
 </main>
