557487da023ffa09a3d084f9668f65abd53127f8
Marcos <marcosrabcob@gmail.com>
2020-07-08 10:15:50 +0200

    total amount

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,31 @@
 <script>
 // components
 import Navbar from './Navbar.svelte';
-import ExpensesList from './ExpensesList.svelte'
+import ExpensesList from './ExpensesList.svelte';
+import Totals from "./Totals.svelte";
 // data
 import expensesData from '../public/expenses';
-
 // variables 
 let expenses = [...expensesData];
-
+// reactive
+$: total = expenses.reduce((acc,curr)=>{
+    console.log({acc,amount:curr.amount});
+    return (acc += curr.amount);
+},0)
 //funtions
 function removeExpense(id) {
     expenses = expenses.filter(item => item.id !== id);
 
 }
+
+function clearExpenses(){
+    expenses = [];
+}
 </script>
 
 <Navbar />
 <main class="content">
+    <Totals title = "total expenses" {total}/>
     <ExpensesList {expenses} {removeExpense}/>
+    <button class="btn btn-primary btn-block" on:click={clearExpenses}>clear expenses</button>
 </main>
---
+++ src/Totals.svelte
@@ -0,0 +1,7 @@
+<script>
+export let title = 'default tietle';
+export let total = 0;
+</script>
+<section class="main-title">
+<h2>{title}: ${total}</h2>
+</section>
